12334ff281df47146d507a1e96ac0e97e5ae0f5d
Danilo Duarte <danilooduarte@gmail.com>
2020-04-22 23:08:00 -0300

    CRUD Scaffolding

---
+++ SalesWeBMvc/Data/SalesWeBMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWeBMvc.Models
+{
+    public class SalesWeBMvcContext : DbContext
+    {
+        public SalesWeBMvcContext (DbContextOptions<SalesWeBMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWeBMvc.Models.Department> Department { get; set; }
+    }
+}
