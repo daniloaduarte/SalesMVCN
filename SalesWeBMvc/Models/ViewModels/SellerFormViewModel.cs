4eca51075718c334ec34e4ce4a19ed0392f5afb4
Danilo Duarte <danilooduarte@gmail.com>
2020-04-29 13:24:50 -0300

    SellerFormViewModel incompleto

---
+++ SalesWeBMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeBMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+    }
+}
